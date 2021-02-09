d4074fe229e3df04e33a680754ac15b43e184382
Jarett <allieja@mail.gvsu.edu>
2021-02-08 21:01:02 -0500

    T search

--- Maze_Solver.py
+++ Maze_Solver.py
@@ -9,38 +9,77 @@ class MazeSolver(object):
     def __init__(self, world):
         self.world = world
 
-        self.bfs_discovered = {}
-        self.bfs_queue = []
+        #T search
+        self.t_discovered = {}
+        self.t_discovered_twice = {}
+        self.t_queue = []
+        self.t_reverse = []
 
-    def bfs_search(self):
-        """ Conduct a BFS of the maze. """
-        # Initialize the BFS
-        self.bfs_discovered[self.world.player] = 'root'
-        self.bfs_queue.append(self.world.player)
-
-        # Set the color of the cell since it has been discovered
+    def t_search(self):
+        self.t_discovered[self.world.player] = 'root'
+        self.t_queue.append(self.world.player)
+        self.t_reverse.append(self.world.player)
         self.world.set_cell_discovered(self.world.player)
- 
-        while len(self.bfs_queue) > 0:
-            node = self.bfs_queue.pop(0)
+
+        while len(self.t_queue) > 0:
+            node = self.t_queue.pop(0)
+
             if self.world.check_finish_node(node):
+                print(node)
                 return node
+  
+            
 
-            # If not the goal, process the cell and adjacent valid move to cells.
-            self.world.set_cell_visited(node)
             for n in [(node[0]-1, node[1]), (node[0]+1, node[1]), (node[0], node[1]-1), (node[0], node[1]+1)]:
-                if self.world.check_valid_move_cell(n) and n not in self.bfs_discovered:
-                    self.bfs_discovered[n] = node
-                    self.bfs_queue.append(n)
-
+                if  self.world.check_valid_move_cell(n) and n not in self.t_discovered:
+                    self.t_discovered[n] = node
+                    self.t_queue.append(n)     
+                    self.t_reverse.append(n) 
                     # Set the color of the cell since it has been discovered.
                     self.world.set_cell_discovered(n)
                     # Sleep in order to show the exploration.
                     # Comment line out if you want it to just go.
                     # Adjust float value lower if you want it to go faster.
-                    time.sleep(0.05)
+                    time.sleep(0.15)
+                    break
+                else:
+                    if self.stuck(node):
+                        new_node = self.t_reverse.pop(len(self.t_reverse)-1)
+                        self.t_queue.append(new_node) 
+                        self.t_discovered_twice[n] = new_node
+                        self.world.set_cell_visited_twice(new_node) 
+                        time.sleep(0.15)
+                        break
 
-    def bfs_path(self, end):
+                    
+                        
+                    
+    def stuck(self,n):
+
+        num_stuck = 0
+
+        if(not self.world.check_valid_move_cell((n[0]-1,n[1])) or (n[0]-1,n[1]) in self.t_discovered_twice or (n[0]-1,n[1]) in self.t_discovered):
+            num_stuck += 1
+
+        if(not self.world.check_valid_move_cell((n[0]+1,n[1])) or (n[0]+1,n[1]) in self.t_discovered_twice or (n[0]+1,n[1]) in self.t_discovered):
+            num_stuck += 1
+
+        if(not self.world.check_valid_move_cell((n[0],n[1]-1)) or (n[0],n[1]-1) in self.t_discovered_twice or (n[0],n[1]-1) in self.t_discovered):
+            num_stuck += 1
+
+        if(not self.world.check_valid_move_cell((n[0],n[1]+1)) or (n[0],n[1]+1) in self.t_discovered_twice or (n[0],n[1]+1) in self.t_discovered):
+            num_stuck += 1
+
+        if(num_stuck == 4):
+            return True
+        return False
+                        
+            
+
+
+
+
+    def t_path(self, end):
         """ Construct the path to traverse the maze. 
 
         Args:
@@ -48,8 +87,8 @@ class MazeSolver(object):
         """
         path = [end]
 
-        while self.bfs_discovered[path[-1]] != 'root':
-            path.append(self.bfs_discovered[path[-1]])
+        while self.t_discovered[path[-1]] != 'root':
+            path.append(self.t_discovered[path[-1]])
 
         path.reverse()
         return path
@@ -74,8 +113,8 @@ class MazeSolver(object):
         time.sleep(1)
         t = 1
 
-        goal = self.bfs_search()
-        path = self.bfs_path(goal)
+        goal = self.t_search()
+        path = self.t_path(goal)
 
         # Print out the path to the console.
         # Comment out if you don't need it.
@@ -120,9 +159,12 @@ class MazeSolver(object):
                 # MODIFY THIS SLEEP IF THE GAME IS GOING TOO FAST.
                 time.sleep(0.1)
 
+
+
+
 # Get command line arguments needed for the algorithm.
 parser = argparse.ArgumentParser()
-parser.add_argument("--world_file", type=str, default="world_enhanced.dat", help="World file in the worlds folder to use.")
+parser.add_argument("--world_file", type=str, default="demo_test.dat", help="World file in the worlds folder to use.")
 args = parser.parse_args()
 
 # Create a world to render the maze visually and allow 
